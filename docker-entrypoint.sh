785afb90d614294a372eae6976cee1443dacd0a4
Mitch Seymour <mitchseymour@gmail.com>
2019-07-24 11:11:00 -0400

    Simplify docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,32 +1,10 @@
 #!/bin/env bash
 
-# Create the KSQL server configuration file
-if [ -z "$QUERIES_FILE" ]; then
-# No query file was specified, so run KSQL in interactive mode
-cat <<EOF >/etc/ksql/server.properties
-bootstrap.servers=$BOOTSTRAP_SERVERS
-listeners=http://localhost:8088
-EOF
-else
-# A query file was specified, so run KSQL in headless mode
-cat <<EOF >/etc/ksql/server.properties
-bootstrap.servers=$BOOTSTRAP_SERVERS
-ksql.queries.file=$QUERIES_FILE
-EOF
-fi
-
-# Optionally (dev only), run a local Kafka instance.
-#
-# Note: We do not run this as a separate pod in a local env since KSQL can't make sense
-# of the other pod's hostname (even though it's reachable from the KSQL pod)
-[ -n "$RUN_KAFKA_LOCALLY" ] && echo "Running local instance of Kafka" && confluent local start kafka
-[ -n "$CREATE_TOPIC" ] && kafka-topics --zookeeper localhost:2181 --create --topic $CREATE_TOPIC --partitions 4 --replication-factor 1
+[ -n "$INIT_SCRIPT" ] && bash $INIT_SCRIPT
 
 # Only export the JMX_PORT after Kafka has been (optionally) started. Otherwise, there will be port conflicts
 export JMX_PORT="$KSQL_JMX_PORT"
-export KSQL_OPTS="$KSQL_OPTS -javaagent:/opt/ksql/jmx_prometheus_javaagent.jar=8080:/opt/ksql/jmxexporter/jmx_exporter_config.yaml"
-
-cat /etc/ksql/server.properties
+export KSQL_OPTS="$KSQL_OPTS -javaagent:/opt/ksql/jmx_prometheus_javaagent.jar=8080:/etc/ksql/jmxexporter/jmx_exporter_config.yaml"
 
 KSQL_PID=0
 
