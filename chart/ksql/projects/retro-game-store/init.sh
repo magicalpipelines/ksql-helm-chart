6a1cfc6aea7cb6627db084f0f22cde38a690c306
Mitch Seymour <mitchseymour@gmail.com>
2019-07-25 13:26:03 -0400

    Fix comment placement

--- chart/ksql/projects/retro-game-store/init.sh
+++ chart/ksql/projects/retro-game-store/init.sh
@@ -10,9 +10,9 @@ do
    kafka-topics --zookeeper=localhost:2181 --create --topic "$i" --partitions 4 --replication-factor 1
 done
 
-# use the data generator for the avro data
 echo "generating dummy data"
 # populate the users topic using the console producer
 kafka-console-producer --broker-list localhost:9092 --property 'parse.key=true' --property 'key.separator=:' --topic users < /etc/ksql/project/users.json
+# use the data generator for the avro data
 ksql-datagen schema=game_purchase.avro format=avro topic=game_purchases key=user_id maxInterval=5000 iterations=100 &>/dev/null &
 ksql-datagen schema=console_purchase.avro format=avro topic=console_purchases key=user_id maxInterval=5000 iterations=100 &>/dev/null &
